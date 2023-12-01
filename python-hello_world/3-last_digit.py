436348e25b6f1ed27b267a45e33cd81085ed1d97
Bostaef <moustafa.hosnyy@gmail.com>
2023-12-01 16:45:14 +0200

    handle tha negative

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,6 @@
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1]) 
+last_digit = int(str(abs(number))[-1]) * (1 if number >= 0 else -1) 
 print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
     print("and is greater than 5")
