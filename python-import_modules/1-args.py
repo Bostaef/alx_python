dbd1acb816b49ccd1dd6e8a7170992bb7affbae2
Bostaef <moustafa.hosnyy@gmail.com>
2023-12-07 22:44:49 +0200

    1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,10 @@
 def print_arguments(arguments):
     num_args = len(arguments) - 1
 
-    result = "{} argument{}:".format(num_args, "s" if num_args != 1 else "")
-
     if num_args == 0:
-        result += "."
+        result = "0 arguments."
     else:
-        result += "\n"
+        result = "{} argument{}:\n".format(num_args, "s" if num_args != 1 else "")
         for i in range(1, num_args + 1):
             result += "{}: {}\n".format(i, arguments[i])
 
@@ -14,10 +12,10 @@ def print_arguments(arguments):
 
 if __name__ == "__main__":
     # Example usage:
-    command_line_arguments = ["", "Hello"]
+    command_line_arguments = ["", "Hello", "Holberton", "School", "98", "Battery", "street"]
     
     # Call the function to get the formatted output
     output = print_arguments(command_line_arguments)
     
     # Print the result
-    print(output)
+    print(output, end="")
