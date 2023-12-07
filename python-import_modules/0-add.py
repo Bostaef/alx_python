bda05ac8aec886f43836c6012a98e36cc26fb07d
Bostaef <moustafa.hosnyy@gmail.com>
2023-12-07 21:57:05 +0200

    1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
+import importlib
+
 a = 1
 b = 2
 
-result = __import__("add_0", fromlist=["add"]).add(a, b)
+add_0 = importlib.import_module("add_0")
+result = add_0.add(a, b)
 
 print("{:d} + {:d} = {:d}".format(a, b, result))
 
