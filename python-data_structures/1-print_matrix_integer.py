70cc89d503f27a122df26e476209cc2c01e31b4d
Bostaef <moustafa.hosnyy@gmail.com>
2023-12-11 14:30:18 +0200

    1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,9 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for index, element in enumerate(row):
-            print("{:d}".format(element), end="")
-            if index < len(row) - 1:
-                print(" ", end="")
-        print("$")
-    print("--$")
+        for element in row:
+            print("{:d} ".format(element), end="")
 
